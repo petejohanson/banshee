731d9b3b341c2a1b70c272adfb7ad0bf13ce6e7e
Aaron Bockover <abockover@novell.com>
2010-03-09 15:40:00 -0500

    [grid] call InvalidateVirtualSize on Allocate

--- src/Libraries/Hyena.Gui/Hyena.Data.Gui/DataViewLayout.cs
+++ src/Libraries/Hyena.Gui/Hyena.Data.Gui/DataViewLayout.cs
@@ -76,6 +76,7 @@ namespace Hyena.Data.Gui
 
             InvalidateChildSize ();
             InvalidateChildCollection ();
+            InvalidateVirtualSize ();
             InvalidateChildLayout ();
         }
 
