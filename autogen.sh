30088d335e1b97458481fcdf6b2599d2500661f0
Bertrand Lorentz <bertrand.lorentz@gmail.com>
2010-06-08 21:14:17 +0200

    [build] Tell gnome-doc-prepare to be silent

--- autogen.sh
+++ autogen.sh
@@ -64,7 +64,7 @@ check_autotool_version $LIBTOOLIZE 1.4.3
 check_autotool_version intltoolize 0.35.0
 check_autotool_version pkg-config 0.14.0
 
-gnome-doc-prepare
+gnome-doc-prepare --automake
 run intltoolize --force --copy
 run $LIBTOOLIZE --force --copy --automake
 run aclocal -I build/m4/banshee -I build/m4/shamrock -I build/m4/shave $ACLOCAL_FLAGS
