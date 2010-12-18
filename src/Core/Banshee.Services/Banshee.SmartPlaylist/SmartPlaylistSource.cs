2210fe7a13ddec7e248929085470ec9df524cb15
Bertrand Lorentz <bertrand.lorentz@gmail.com>
2010-12-18 15:40:39 +0100

    SmartPlaylistSource: Clear the condition when it's not enabled
    If the ConditionTree is set to null, we need to also clear out the
    ConditionXml and ConditionSql. This prevents any previously existing
    condition from remaining in place. Fixes bgo#637365.

--- src/Core/Banshee.Services/Banshee.SmartPlaylist/SmartPlaylistSource.cs
+++ src/Core/Banshee.Services/Banshee.SmartPlaylist/SmartPlaylistSource.cs
@@ -120,6 +120,9 @@ namespace Banshee.SmartPlaylist
                         if (!relevant_fields.Contains (term.Field))
                             relevant_fields.Add (term.Field);
                     }
+                } else {
+                    condition_sql = null;
+                    condition_xml = null;
                 }
             }
         }
