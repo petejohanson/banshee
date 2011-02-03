0cfa4a6065746c9e18c7a330d2a6856d85ac88cb
Gabriel Burt <gabriel.burt@gmail.com>
2011-02-03 12:08:12 -0600

    windows: Echo message at end of checkout-banshee.bat

--- build/windows/checkout-banshee.bat
+++ build/windows/checkout-banshee.bat
@@ -4,4 +4,4 @@ REM See banshee\build\windows\README.txt for more information
 REM (this file is tracked in version control at: http://git.gnome.org/browse/banshee/plain/build/windows/checkout-banshee.bat )
 
 REM for some reason need to put these all on one line w/ && between; as separate commands on separate lines it stops after a given git cmd
-git clone git://git.gnome.org/banshee && cd banshee && git submodule update --init && git clone git://gitorious.org/banshee/windows-binaries.git bin
+git clone git://git.gnome.org/banshee && cd banshee && git submodule update --init && git clone git://gitorious.org/banshee/windows-binaries.git bin && echo "Checkout script finished. Banshee is now checked out into the banshee folder.  Build it with build\windows\build-banshee.bat or your favorite IDE using Banshee.sln" && pause
