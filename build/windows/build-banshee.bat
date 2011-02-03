704eac76dfa6a66cec32a8b250f8d0e674d81877
Gabriel Burt <gabriel.burt@gmail.com>
2011-02-03 11:57:55 -0600

    windows: add build-banshee and checkout-banshee scripts

---
+++ build/windows/build-banshee.bat
@@ -0,0 +1,2 @@
+cd ..\..
+C:\WINDOWS\Microsoft.NET\Framework\v3.5\msbuild Banshee.sln /p:Configuration=Windows
---
+++ build/windows/checkout-banshee.bat
@@ -0,0 +1,7 @@
+REM This script will checkout Banshee from git (and a few submodules that are needed).
+REM It will result in a banshee/ directory under where it's launched from.
+REM See banshee\build\windows\README.txt for more information
+REM (this file is tracked in version control at: http://git.gnome.org/browse/banshee/plain/build/windows/checkout-banshee.bat )
+
+REM for some reason need to put these all on one line w/ && between; as separate commands on separate lines it stops after a given git cmd
+git clone git://git.gnome.org/banshee && cd banshee && git submodule update --init && git clone git://gitorious.org/banshee/windows-binaries.git bin
