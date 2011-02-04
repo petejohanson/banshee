4adcff18e505c8e0ad05c4380da7c310fd764fbd
Matt Sturgeon <mttza1@gmail.com>
2011-02-04 16:03:04 +0000

    windows: add /p:Platform="Any CPU" to build-banshee.bat (bgo#641513)
    Signed-off-by: Gabriel Burt <gabriel.burt@gmail.com>

--- build/windows/build-banshee.bat
+++ build/windows/build-banshee.bat
@@ -1,2 +1,2 @@
 cd ..\..
-C:\WINDOWS\Microsoft.NET\Framework\v3.5\msbuild Banshee.sln /p:Configuration=Windows
+C:\WINDOWS\Microsoft.NET\Framework\v3.5\msbuild Banshee.sln /p:Configuration=Windows /p:Platform="Any CPU"
